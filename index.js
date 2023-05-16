7aae53c74c456721c3b7972bcca51af28fe767d5
Alexsandro Marciel <alexsandro.marciel@gmail.com>
2023-05-15 21:34:44 -0300

    commit 03

--- index.js
+++ index.js
@@ -8,31 +8,3 @@ function fechar() {
     document.querySelector("div").style.opacity = 1
 }
 
-var draggableElements = document.querySelectorAll('.draggable');
-var draggable;
-
-for (var i = 0; i < draggableElements.length; i++) {
-    draggableElements[i].addEventListener('dragstart', function (event) {
-        draggable = this;
-        event.dataTransfer.setData('text/plain', null);
-    });
-
-    draggableElements[i].addEventListener('drag', function (event) {
-        // Adicione qualquer lógica adicional de arrastar, se necessário
-    });
-}
-
-document.addEventListener('dragover', function (event) {
-    event.preventDefault();
-});
-
-document.addEventListener('drop', function (event) {
-    if (draggable) {
-        event.preventDefault();
-        draggable.style.left = event.pageX + 'px';
-        draggable.style.top = event.pageY + 'px';
-        draggable = null;
-    }
-});
-
-
